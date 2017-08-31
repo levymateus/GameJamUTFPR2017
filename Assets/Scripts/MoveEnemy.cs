d5455e4486e4131f0e40b8c859b54d5356a048c3
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2017-08-31 16:54:50 -0300

    comportamento do inimigo

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float movementSpeed = 1;
+
+	public float leftBoundary;
+
+	public float rightBoundary;
+
+	private bool side;
+
+	public float moveWait;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		side = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move()
+	{
+		if (transform.position.x <= leftBoundary)
+			side = false;
+
+		if (transform.position.x >= rightBoundary)
+			side = true;
+
+		if(side)
+			rb.MovePosition (transform.position - transform.right * movementSpeed * Time.deltaTime);
+		else
+			rb.MovePosition (transform.position + transform.right * movementSpeed * Time.deltaTime);
+		
+	}
+}
